89fbf2b0587f946494faca80e19f6a7c4e7cd50f
mcskinner <git@mcskinner.com>
2018-03-03 09:30:35 -0800

    use placeholder objects to fail faster on bad param combos

--- main.py
+++ main.py
@@ -4,11 +4,48 @@ import functools
 import inspect
 
 
+class Placeholder(object):
+    """Placeholder for a thing that will arrive eventually.
+
+    This is kind of like a Promise but without the chance of failure.
+    """
+
+    def __init__(self, name):
+        self._name = name
+        self._has = False
+        self._val = None
+
+    def get(self):
+        if not self._has:
+            raise ValueError('no value set for `{}`'.format(self._name))
+        return self._val
+
+    def set(self, val):
+        self._has = True
+        self._val = val
+
+    def unset(self):
+        self._has = False
+
+
+def with_placeholders(func):
+    """Wrap a function to accept placeholders for some arguments."""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        args = [arg.get() if isinstance(arg, Placeholder) else arg for arg in args]
+        return func(*args, **kwargs)
+    return wrapper
+
+
 def fn_params(func):
+    """Shorthand to get the parameter spec for a function."""
     return inspect.signature(func).parameters.values()
 
 
 def defer(*peels):
+    if len(peels) != len(set(peels)):
+        raise TypeError('duplicate args in {}'.format(peels))
+
     def decorator(func):
         pos_args = {
             p.name: i for i, p in enumerate(fn_params(func))
@@ -22,27 +59,43 @@ def defer(*peels):
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            def most_inner_fn(*peeled_args):
-                if len(vindex) != len(peeled_args):
-                    raise TypeError('expected {} args, got {}'.format(
-                        len(vindex), len(peeled_args)))
+            my_args = list(args)
 
-                my_args = list(args)
-                for i, peel in zip(vindex, peeled_args):
-                    my_args = my_args[:i] + [peel] + my_args[i:]
-                return func(*my_args, **kwargs)
+            fakes = [Placeholder(peel) for peel in peels]
+            for i, fake in zip(vindex, fakes):
+                my_args = my_args[:i] + [fake] + my_args[i:]
+            thunk = functools.partial(with_placeholders(func), *my_args, **kwargs)
+
+            for param in fn_params(thunk):
+                if param.default is param.empty:
+                    raise TypeError('missing required positional param `{}`'.format(param.name))
+
+            def most_inner_fn(*peeled_args):
+                if len(fakes) != len(peeled_args):
+                    raise TypeError('expected {} args, got {}'.format(
+                        len(fakes), len(peeled_args)))
+
+                for fake, peel in zip(fakes, peeled_args):
+                    fake.set(peel)
+
+                try:
+                    return thunk()
+                finally:
+                    for fake in fakes:
+                        fake.unset()
+
             return most_inner_fn
         return wrapper
     return decorator
 
 
-@defer('a', 'b')
-def some_func(a, b, c=10):
-    print(a + b * c)
+@defer('a')
+def some_func(a, b, c=9):
+    print(100*a + 10*b + c)
 
 
 def main():
-    some_func(100)(2, 3)
+    some_func(1, 2)(3)
 
 
 if __name__ == '__main__':
